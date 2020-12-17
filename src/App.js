4f0410ce8a7dedbe7739fca5e5e334bc1ef0ee2e
ShivangiRai1310 <shivangirai1310@gmail.com>
2020-12-17 22:00:19 +0530

    User authentication & firebase deployment done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Checkout from "./components/Checkout";
 import Login from "./components/Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
 
 function App() {
+
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    //firebase listener
+    //will only run once when th app component loads...like an if stmt in React
+
+    auth.onAuthStateChanged(authUser => {
+      console.log("The user is >> ", authUser);
+
+      if(authUser){
+        //the user just logged in / the user was logged in
+        //everytime a user logs in the data is dipatched to the data layer
+        dispatch({
+          type: "SET_USER",
+          user: authUser
+        });
+      }
+      else{
+        //the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null
+        });
+      }
+    });
+
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
   return (
     <Router>
-      <div className="App">
+      <div className="app">
         <Switch>
 
           <Route path="/login">
