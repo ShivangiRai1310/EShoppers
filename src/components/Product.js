71960a3a0db470e4645863468f9671903236a440
SandhyaTandon <sandhyakrishnat@gmail.com>
2020-12-29 12:39:37 +0530

    category details added

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Product.css";
 import { Link } from "react-router-dom";
 import { useStateValue } from "./StateProvider";
+import { MDBRow, MDBCol, MDBCard, MDBCardImage, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardFooter, MDBBtn } from 'mdbreact';
 
 function Product({ id, data }) {
   const [{ basket }, dispatch] = useStateValue();
@@ -30,8 +31,54 @@ function Product({ id, data }) {
 
   return (
     
-      <div className="product">
-        <div className="product-info">
+      <section className='text-center my-5'>
+          <MDBRow>
+            <MDBCol lg='4' md='12' className='mb-lg-0 mb-4'>
+              <MDBCard wide ecommerce>
+                <MDBCardImage
+                  cascade
+                  src={data.image}
+                  top
+                  alt='product'
+                />
+                <MDBCardBody cascade className='text-center'>
+                  <MDBCardTitle>
+                      <p>{data.title[0]}</p>
+                  </MDBCardTitle>
+                  <MDBCardText>{data.title[1]}</MDBCardText>
+                  <MDBCardFooter className='px-1'>
+                    <span className='float-left font-weight-bold'>
+                      <strong>₹{data.price}</strong>
+                    </span>
+                    <span className='float-right'>
+                      {/* <MDBTooltip domElement placement='top'> */}
+                        <div className="product-rating">
+                          {Array(data.rating)
+                            .fill()
+                            .map((_, i) => (
+                              <p>⭐</p>
+                            ))}
+                        </div>
+                      {/* </MDBTooltip> */}
+                      {/* <MDBTooltip domElement placement='top'>
+                        <i className='grey-text fa fa-shopping-cart' />
+                        <span>Add to cart</span>
+                      </MDBTooltip> */}
+                    </span>
+                    <span className='float-left'>
+                      <MDBBtn onClick={addToBasket} color="primary">Add to basket</MDBBtn>
+                    </span>
+                    <span className='float-right'>
+                      <Link to={newTo}>
+                        <MDBBtn color="secondary">View</MDBBtn>
+                      </Link> 
+                    </span>                    
+                  </MDBCardFooter>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+          </MDBRow>
+        {/* <div className="product-info">
           <p>{data.title}</p>
           <p className="product-price">
             <small>₹</small>
@@ -49,8 +96,8 @@ function Product({ id, data }) {
         <img src={data.image} alt="product" />
 
         <button onClick={addToBasket}>Add to Basket</button>
-        <Link to={newTo}><button>More Details</button></Link>
-      </div>
+        <Link to={newTo}><button>More Details</button></Link> */}
+      </section>
   );
 }
 
