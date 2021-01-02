06ac9598e1ef66cdbdf0daa6836ce2d0eba88866
SandhyaTandon <sandhyakrishnat@gmail.com>
2021-01-02 15:27:39 +0530

    small changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,37 +14,37 @@ function Home() {
       <Carousel>
         <Carousel.Item>
           <img
-            className="d-block w-100"
+            className="d-block w-100 carousel-img"
             src="https://www.indiumsoftware.com/blog/wp-content/uploads/2018/04/5-ways-big-data-in-e-commerce.jpg"
             alt="First slide"
           />
           <Carousel.Caption>
-            <h3>Discover something new</h3>
-            <p>Experience the difference.Experience the lifestyle.</p>
+            <h1>Discover something new</h1>
+            <p className="h3-responsive font-weight-bold">Experience the difference.Experience the lifestyle.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className="d-block w-100"
+            className="d-block w-100 carousel-img"
             src="https://www.informaticsinc.com/application/files/6615/1458/2953/5topecommerceplatformsonthemarket-900.jpg"
             alt="Third slide"
           />
 
           <Carousel.Caption>
-            <h3>A fresh approach to shopping</h3>
-            <p>An exciting place for the whole family to shop.</p>
+            <h1>A fresh approach to shopping</h1>
+            <p className="h3-responsive font-weight-bold">An exciting place for the whole family to shop.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className="d-block w-100"
+            className="d-block w-100 carousel-img"
             src="https://www.indiumsoftware.com/blog/wp-content/uploads/2019/10/Performance-testing-for-eCommerce-websites.jpg"
             alt="Third slide"
           />
 
           <Carousel.Caption>
-            <h3>Always ahead</h3>
-            <p>Favorite brands and hottest trends.Find it, love it, buy it.</p>
+            <h1>Always ahead</h1>
+            <p className="h3-responsive font-weight-bold">Favorite brands and hottest trends.Find it, love it, buy it.</p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
